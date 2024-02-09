1b84e749e018347d61c4f9cccede1a482a4e1c05
Vordgi <sasha2822222@gmail.com>
2024-02-09 21:25:52 +0400

    yph-2 add check that hook exist

--- install.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const { execSync } = require('child_process');
-const { version } = require('./package.json');
-
-const command = `yarn plugin import https://github.com/vordgi/yarn-plugin-hooker/releases/download/${version}/yarn-plugin-hooker.js`;
-execSync(command, { shell: true, stdio: 'inherit' });
